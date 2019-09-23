83f713d0e6d701d9fbe67c5329a7398ddce543e2
Frank.Fung <frankho0914@outlook.com>
2019-09-23 14:06:56 +0800

    11-实现文章列表数据的分页展示-limit关键字的使用

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,8 +1,15 @@
 $(function () {
+  var pageSize = 2,
+    pageNum = 2
+    // pageSize:每页显示的记录数
+    // pageNum:当前页码
   $.ajax({
     type: 'get',
     url: '/getPostList',
-    data: {},
+    data: {
+      pageSize,
+      pageNum
+    },
     dataType: 'json',
     success: function (res) {
       console.log(res);
