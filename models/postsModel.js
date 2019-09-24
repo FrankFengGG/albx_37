348c2bfb370059835dea4cab74e52bec5cdc5a04
Frank.Fung <frankho0914@outlook.com>
2019-09-24 21:25:13 +0800

    16-实现文章的新增

--- models/postsModel.js
+++ models/postsModel.js
@@ -27,26 +27,25 @@ module.exports = {
               join categories on posts.category_id=categories.id
               where 1=1` //恒成立
 
-              // 拼接筛选条件
-              if(query.cate && query.cate != 'all'){ //说明有分类条件
-                sql += `and posts.category_id = ${query.cate}`
-              }
-              if(query.statu && query.statu != 'all' ){
-                sql += `and posts.status = ${query.statu}`
-              }
-
-              sql += `order by posts.id DESC
-              limit ${(query.pageNum-1)*query.pageSize},${query.pageSize}`
+    // 拼接筛选条件
+    if (query.cate && query.cate != 'all') { //说明有分类条件
+      sql += `and posts.category_id = ${query.cate}`
+    }
+    if (query.statu && query.statu != 'all') {
+      sql += `and posts.status = ${query.statu}`
+    }
+    sql += `order by posts.id DESC
+     limit ${(query.pageNum-1)*query.pageSize},${query.pageSize}`
     conn.query(sql, (err, results) => {
       if (err) {
         callback(err)
       } else {
         // 再次创建sql语句 获取总记录数
         sql = 'select count(*) as cnt from posts where 1=1'
-        if(query.cate && query.cate != 'all'){ //说明有分类条件
+        if (query.cate && query.cate != 'all') { //说明有分类条件
           sql += `and posts.category_id = ${query.cate}`
         }
-        if(query.statu && query.statu != 'all' ){
+        if (query.statu && query.statu != 'all') {
           sql += `and posts.status = ${query.statu}`
         }
         conn.query(sql, (err2, results2) => {
@@ -60,6 +59,24 @@ module.exports = {
             })
           }
         })
+      }
+    })
+  },
+
+  addPost(obj, callback) {
+    let sql = `insert into posts values
+    (null,'${obj.slug}','${obj.title}','${obj.feature}','${obj.created}','${obj.content}','${obj.views}','${obj.likes}','${obj.status}','${obj.user_id}','${obj.category}')`
+    // 在mysql第三方模块中，提供了参数化查询的方式，就是使用?作为参数占位符简化sql语句
+    // 它可以自动的生成sql语句
+    let sql = 'insert into posts set ?'
+    // 如果参数是对象，就不用使用[]包含
+    // 如果是单独的值，就需要使用[]包含
+    conn.query(sql, obj, (err) => {
+      if (err) {
+        console.log(err);
+        callback(err);
+      } else {
+        callback(null)
       }
     })
   }
